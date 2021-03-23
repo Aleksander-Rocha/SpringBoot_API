eeab587f6b26147eaf466e6c79e4858644610954
Aleksander Rocha <60040470+Aleksander-Rocha@users.noreply.github.com>
2021-03-23 00:30:49 -0300

    UP

---
+++ src/main/java/com/spring/crm/controller/ClienteController.java
@@ -0,0 +1,33 @@
+package com.spring.crm.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.crm.model.Cliente;
+import com.spring.crm.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/clientes")
+
+public class ClienteController {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@GetMapping
+	public List<Cliente> listar() {
+		return clienteRepository.findAll();
+	}
+	
+	@PostMapping
+	public Cliente adicionar(@RequestBody Cliente cliente){
+		return clienteRepository.save(cliente);
+	}
+	
+	
+}
